0a5714d311cb017fcd636da4024ab1f46efc25f3
AlanbusMaker <138153752+AlanbusMaker@users.noreply.github.com>
2025-02-07 12:10:25 -0300

    Add files via upload

--- principalscript.js
+++ principalscript.js
@@ -1,4 +1,4 @@
-const windows = Array.from(document.querySelectorAll(".window, .window2"));
+const windows = Array.from(document.querySelectorAll(".window, .window2, .window3"));
 const textam = document.getElementById("OpenTextam");
 
 let isDragging = false;
@@ -6,9 +6,9 @@ let currentElement = null;
 let offsetX, offsetY;
 
 function onMouseDown(e) {
-    if (windows.includes(e.target.closest('.window, .window2'))) {
+    if (windows.includes(e.target.closest('.window, .window2, .window3'))) {
         isDragging = true;
-        currentElement = e.target.closest('.window, .window2');
+        currentElement = e.target.closest('.window, .window2, .window3');
         offsetX = e.clientX - currentElement.getBoundingClientRect().left;
         offsetY = e.clientY - currentElement.getBoundingClientRect().top;
     }
@@ -36,4 +36,28 @@ document.addEventListener("mouseup", onMouseUp);
 textam.addEventListener('click', () => {
     const textam2 = document.getElementById("textam2");
     textam2.style.display = 'block';
+});
+
+
+function createPC() {
+      // Obtém os valores dos campos
+      var pcName = document.getElementById('PCname').value;
+      var os = document.getElementById('OS').value;
+      var pcType = document.getElementById('PCtype').value;
+
+      // Cria uma nova div para o PC
+      var newPC = document.createElement('div');
+      newPC.innerHTML = '<p>PC Name: ' + pcName + '</p>' +
+                        '<p>Operating System: ' + os + '</p>' +
+                        '<p>PC Type: ' + pcType + '</p>';
+
+      // Adiciona a nova div dentro da div com id "Computers"
+      document.getElementById('Computers').appendChild(newPC);
+}
+
+const internet = document.getElementById("internetcon");
+const internetwin = document.getElementById("internetwin");
+
+internet.addEventListener('click', () => {
+    internetwin.style.display = 'block';
 });
